0ea01e0f2f4a6909f4d0c6fcf9b5f0d5bfdcd77c
Vladimir <bukhtavladimir@gmail.com>
2021-12-18 23:01:33 +0300

    SumOfDigit

--- DigitalRoot.js
+++ DigitalRoot.js
@@ -8,19 +8,39 @@ The input will be a non-negative integer.*/
 //В процессе решения
 
 function digital_root(n) {
+
     let intArr = n.toString().split("");
+    let k = n;
     let lengthArr = intArr.length;
     let newArr;
-
-
-
-
-    const sumOfDigit = (arg) =>{
-        let argArr = arg.toString().split("");
-        let sum;
-        for (let i = 0; i < argArr.length){
-            sum += argArr[i];
-        }
-        return sum;
+    while (sumOfDigit(k) > 9 ){
+        k = sumOfDigit(k)
     }
+    let z = sumOfDigit(k);
+    return z;
 }
+
+function sumOfDigit(arg) {
+    let argArr = arg.toString().split("");
+    let sum = 0;
+    for (let i = 0; i < argArr.length; i++){
+        sum += Number(argArr[i]);
+    }
+    return sum;
+}
+/*
+________________
+function digital_root(n) {
+    return (n - 1) % 9 + 1;
+}________________
+function digital_root(n) {
+    if (n < 10) return n;
+
+    return digital_root(
+        n.toString().split('').reduce(function(acc, d) { return acc + +d; }, 0));
+}
+________________
+function digital_root(n) {
+    return--n%9+1;
+}
+________________
